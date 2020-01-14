f86aacf4cec75a0136a4100a3ff24968c2b8fe5e
savegnago <savegdesigner@gmail.com>
2020-01-14 15:24:21 -0300

    js done

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ const dog_card_name = document.querySelector('#adopted-dog-name')
 const dog_card_breed = document.querySelector('#adopted-dog-breed')
 const dog_api_image = document.querySelector('#dog-api-image')
 const adopt_submit_button = document.querySelector('#adopt-submit')
+const save_card_button = document.querySelector('#save-card-button')
+var save_date_span = document.querySelector('#save-date')
 
 
 // Fetching API List to fill breed input
@@ -50,6 +52,7 @@ form.addEventListener('submit', (e) =>{
 
     e.preventDefault()
 
+    save_date_span.innerHTML = ''
     error_span.style.display = 'none'
 
     var dog_name = dog_name_input.value
@@ -81,6 +84,7 @@ form.addEventListener('submit', (e) =>{
         dog_card_name.style.color = favorite_color
         dog_card_name.style.fontFamily = font
         dog_card_breed.innerHTML = dog_breed
+        save_card_button.style.display = 'block'
 
         adopt_submit_button.value = 'ADOTADO'
         adopt_submit_button.classList.add('adopted-button-style')
@@ -93,3 +97,43 @@ form.addEventListener('submit', (e) =>{
     }
 
 })
+
+// Save Card
+save_card_button.addEventListener('click', () => {
+
+    var dog_name = dog_name_input.value
+    var dog_breed = breeds_input.options[breeds_input.selectedIndex].value
+    var favorite_color = fav_colors_input.options[fav_colors_input.selectedIndex].value
+    var font = fonts_input.options[fonts_input.selectedIndex].value
+    var dog_image_src = dog_api_image.src
+    var date = new Date()
+
+    localStorage.setItem('dog_image', dog_image_src)
+    localStorage.setItem('dog_name', dog_name)
+    localStorage.setItem('dog_breed', dog_breed)
+    localStorage.setItem('favorite_color', favorite_color)
+    localStorage.setItem('font', font)
+    localStorage.setItem('date', date)
+
+    save_date_span.innerHTML = date
+
+})
+
+// On page loading fill inputs with localstorage info (if there are any)
+window.onload = () =>{
+
+    var local_date = localStorage.date
+
+    if(local_date){
+        dog_api_image.src = localStorage.dog_image
+
+        dog_card_name.innerHTML = localStorage.dog_name
+        dog_card_name.style.fontFamily = localStorage.font
+        dog_card_name.style.color = localStorage.favorite_color
+        dog_card_breed.innerHTML = localStorage.dog_breed
+
+        save_date_span.innerHTML = localStorage.date
+
+    }
+
+}
